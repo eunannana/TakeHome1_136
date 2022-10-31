e32b44205f1b79bc4813d894c0644de6bfc17c54
Annisa Divayu Andriyani <95303337+eunannana@users.noreply.github.com>
2022-10-31 10:47:05 +0700

    modify myController.java

--- src/main/java/ws/a/takehome1/myController.java
+++ src/main/java/ws/a/takehome1/myController.java
@@ -4,10 +4,25 @@
  */
 package ws.a.takehome1;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 /**
  *
  * @author eunannana
  */
+@Controller
 public class myController {
-    
+
+    @RequestMapping("/nextpage")
+    public String next(@RequestParam(value = "thename") String itsname,
+            @RequestParam(value = "thelocation") String itslocation,
+            Model courier) {
+        courier.addAttribute("package1", itsname);
+        courier.addAttribute("package2", itslocation);
+        return "viewpage";
+    }
+
 }
